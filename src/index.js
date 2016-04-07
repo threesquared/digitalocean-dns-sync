6d3d176368209ab6a9133bb44e0afc37265e770f
Ben Speakman <ben.speakman@pod-point.com>
2016-04-07 17:25:49 +0100

    Quick updates

--- src/index.js
+++ src/index.js
@@ -35,37 +35,51 @@ glob(pattern, (error, files) => {
                 api.domainRecordsGetAll(domain, { includeAll: true }, (error, response, body) => {
 
                     // Delete default records
-                    body.domain_records.forEach((record) => {
+                    body.forEach((record) => {
                         apiCall('domainRecordsDelete', domain, record.id);
                     });
 
-                    zone.ns.forEach((item) => {
-                        apiCall('domainRecordsCreate', domain, { type: 'NS', name: item.name, data: item.host });
-                    });
+                    if (zone.hasOwnProperty('ns')) {
+                        zone.ns.forEach((item) => {
+                            apiCall('domainRecordsCreate', domain, { type: 'NS', name: item.name, data: item.host });
+                        });
+                    }
 
-                    zone.mx.forEach((item) => {
-                        apiCall('domainRecordsCreate', domain, { type: 'MX', name: item.name, data: item.host, priority: item.preference });
-                    });
+                    if (zone.hasOwnProperty('mx')) {
+                        zone.mx.forEach((item) => {
+                            apiCall('domainRecordsCreate', domain, { type: 'MX', name: item.name, data: item.host, priority: item.preference });
+                        });
+                    }
 
-                    zone.cname.forEach((item) => {
-                        apiCall('domainRecordsCreate', domain, { type: 'CNAME', name: item.name, data: item.alias });
-                    });
+                    if (zone.hasOwnProperty('cname')) {
+                        zone.cname.forEach((item) => {
+                            apiCall('domainRecordsCreate', domain, { type: 'CNAME', name: item.name, data: item.alias });
+                        });
+                    }
 
-                    zone.a.forEach((item) => {
-                        apiCall('domainRecordsCreate', domain, { type: 'A', name: item.name, data: item.ip });
-                    });
+                    if (zone.hasOwnProperty('a')) {
+                        zone.a.forEach((item) => {
+                            apiCall('domainRecordsCreate', domain, { type: 'A', name: item.name, data: item.ip });
+                        });
+                    }
 
-                    zone.aaaa.forEach((item) => {
-                        apiCall('domainRecordsCreate', domain, { type: 'AAAA', name: item.name, data: item.ip });
-                    });
+                    if (zone.hasOwnProperty('aaaa')) {
+                        zone.aaaa.forEach((item) => {
+                            apiCall('domainRecordsCreate', domain, { type: 'AAAA', name: item.name, data: item.ip });
+                        });
+                    }
 
-                    zone.txt.forEach((item) => {
-                        apiCall('domainRecordsCreate', domain, { type: 'TXT', name: item.name, data: item.txt });
-                    });
+                    if (zone.hasOwnProperty('txt')) {
+                        zone.txt.forEach((item) => {
+                            apiCall('domainRecordsCreate', domain, { type: 'TXT', name: item.name, data: item.txt });
+                        });
+                    }
 
-                    zone.srv.forEach((item) => {
-                        apiCall('domainRecordsCreate', domain, { type: 'SRV', name: item.name, data: item.target, priority: item.priority, weight: item.weight, port: item.port });
-                    });
+                    if (zone.hasOwnProperty('srv')) {
+                        zone.srv.forEach((item) => {
+                            apiCall('domainRecordsCreate', domain, { type: 'SRV', name: item.name, data: item.target, priority: item.priority, weight: item.weight, port: item.port });
+                        });
+                    }
 
                 });
             });
